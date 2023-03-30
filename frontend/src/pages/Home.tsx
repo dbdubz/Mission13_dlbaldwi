8b3159c81d18c0924aceb62eba7a94af4aa28824
Dalyn Baldwin <101284161+dbdubz@users.noreply.github.com>
2023-03-30 08:55:54 -0600

    Created page descriptions

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import image from '../JoelHiltonHeadshot.jpg';
 
 export default function Home() {
@@ -6,6 +7,13 @@ export default function Home() {
     <div>
       <center>
         <img src={image} alt="Joel Hilton"></img>
+        <p>
+          This website highlights movies from{' '}
+          <Link to="/movies">Joel Hilton's collection</Link>
+        </p>
+        <p>
+          You can also check out his podcast <Link to="/podcasts">here</Link>
+        </p>
       </center>
     </div>
   );
